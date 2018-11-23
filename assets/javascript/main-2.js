3b303a24fbba16ff35d721c350787a3c40dfc71f
LACH <laurachg8@gmail.com>
2018-11-23 16:39:05 -0600

    Added comments and refined code

--- assets/javascript/main-2.js
+++ assets/javascript/main-2.js
@@ -1,183 +1,239 @@
-var token = '';
-var r;
+var token = ''; // token stored in global variable as it is needed for Playback SDK
+var r; // temporary for response in AJAX call
+
 $(document).ready(() => {
-$('.users-menu-icon, .close-menu-icon').click(function () {
-$('.users-menu-icon, .close-menu-icon, .users').toggleClass('hidden');
-$('.users-menu').toggleClass('menu-opened');
+
+    /*If room was previously closed (admin left),
+    room gets deleted from database, so it has to be
+    created all over again. Class is added to add
+    effect and 'Room is closed' notification is
+    displayed. */
+    if (sessionStorage.getItem('closed') === 'yes') {
+        var div = $('.room-closed');
+        div.empty();
+        div.addClass('over');
+        var h3 = $('<h4>');
+        var p = $('<p>');
+        h3.text('Room is closed.');
+        p.text('Administrator left.');
+        div.append(h3, p);
+    }
+    /* Menu with list of users. Toggles hiding class to alternate
+    between open and closed menu. */
+    $('.users-menu-icon, .close-menu-icon').click(function () {
+        $('.users-menu-icon, .close-menu-icon, .users').toggleClass('hidden');
+        $('.users-menu').toggleClass('menu-opened');
+
+    });
+
+    //Database initialization
+    var config = {
+        apiKey: "AIzaSyBx9Tndm3dCv-AKVPS_pslFRAWz3je1_7g",
+        authDomain: "project-1-ec39b.firebaseapp.com",
+        databaseURL: "https://project-1-ec39b.firebaseio.com",
+        projectId: "project-1-ec39b",
+        storageBucket: "project-1-ec39b.appspot.com",
+        messagingSenderId: "90657483363"
+    };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database(); // stores firebase database
+
+    var connected = database.ref(".info/connected"); // reference to directory in firebase database with online connections
+
+    var name = sessionStorage.getItem('name'); // get name value stored in previous page from session storage
+    var room = sessionStorage.getItem('room'); // get room value stored in previous page from session storage
+    var admin = false; // administrator flag
+
+
+    /* If the 'created' flag is 'yes', it means
+    the room was created and the user is the admin */
+    if (sessionStorage.getItem('created') === 'yes') {
+        admin = true;
+    }
+
+    var current_room = database.ref('/rooms/' + room); // reference to user's specified room in database
+
+    /* Any change in the online connections reference,
+    triggers an addition or deletion of the user to
+    the specified room.
+    (1) If the user is the admin, it is added with the admin key.
+    (2) If the admin gets disconnected, it is removed from user list and room is closed.
+    */
+    connected.on("value", function (snapshot) {
+        if (snapshot.val() && sessionStorage.getItem('closed') != 'yes') {
+            if (admin) {
+                current_room.set({
+                    'admin': name
+                });
+                var con = current_room;
+                con.onDisconnect().remove();
+
+            }
+            else {
+                var con = current_room.push(name);
+                con.onDisconnect().remove();
+            }
+
+        }
+    });
+
+    /* If any child from this room removed, in other words,
+    a user leaves the room, the function is triggered.
+    (1) If the user that left is the admin, the room gets closed.
+    (2) A 'closed' flag is stored in session storage.
+    (3) Name of user is found in users list and removed. */
+
+    current_room.on("child_removed", function (snapshot) {
+        if (snapshot.ref_.path.pieces_[2] === 'admin') {  // (1)
+            var div = $('.room-closed');
+            div.empty();
+            div.addClass('over');
+            var h3 = $('<h4>');
+            var p = $('<p>');
+            h3.text('Room is closed.');
+            p.text('Administrator left.');
+            div.append(h3, p);
+
+            sessionStorage.setItem('closed', 'yes'); // (2)
+        }
+
+        $('.users p:contains("' + snapshot.val() + '")').remove(); // (3)
+
+    })
+
+    /* If any child is added to this room, in other words,
+        a user enters the room, the function is triggered.
+        (1) Name previously stored is stored in variable.
+        (2) Create and add text to element to display variable.
+        (3) Name of user is appended to users list. */
+    current_room.on("child_added", function (snapshot) {
+        var name = snapshot.val(); // (1)
+
+        var p = $('<p>'); // (2)
+
+        p.text(name); // (2)
+
+        $('.users').append(p); // (3)
+    });
+
+    // Click event handler
+    $('#login').click(function () {
+        /* Login URL with client ID gotten for API
+        and redirect URI from whitelist specified
+        for app */
+        location.href = 'https://accounts.spotify.com/authorize/?client_id=537b0cdacc7f46de8bd9bf27057c93ca&response_type=token&redirect_uri=https://chapaglaura.github.io/Project-1/player.html';
+    })
+
+    /*Gets current URL, as it is needed if the
+    access token has been given. The Spotify API
+    needs an access token to access its database. 
+    (1) Gets current URL
+    (2) If current URL has access token, URL is split until token is left*/
+
+    var current_url = $(location).attr('href'); // (1)
+
+    if (current_url.includes('access_token')) { // (2)
+        current_url_array = current_url.split('=');
+        current_url_array = current_url_array[1].split('&');
+        token = current_url_array[0];
+
+    }
+
+    // Click event handler
+    $('.search-btn').click(function () {
+
+        /* When choosing categories for search query:
+        (1) Selected category is stored in variable
+        (2) Input element has attributes id and placeholder set to category chosen
+        (3) Change active class from previous category selected to current one */
+
+        var clicked = $(this).text(); // (1)
+
+        $('.search-parameter').attr({ 'id': clicked.toLowerCase(), 'placeholder': clicked }); // (2)
+
+        $('.search-btn').removeClass('active'); // (3)
+        $(this).addClass('active');
+    });
+
+    var selected;
+    $('.final-search-btn').click(function () {
+        selected = $('.search-btn.active').text().toLowerCase();
+        var query = $('.search-parameter').val();
+        $('.search-parameter').val('');
+        query = query.split(' ').join('+');
+
+        $('.search-results').empty();
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/search?q=' + query + '&type=' + selected + '&access_token=' + token,
+            method: 'GET'
+        }).then(function (response) {
+            r = { ...response };
+            console.log(r);
+            selected += 's';
+
+            getSelected(selected, response);
+        });
+
+
+    });
+
+    $('.search-results').on('click', '.each-search', function (e) {
+        e.preventDefault();
+        console.log("HEY");
+        var index = $(this).attr('data-number');
+    });
+
+
 
 });
 
-var config = {
-apiKey: "AIzaSyBx9Tndm3dCv-AKVPS_pslFRAWz3je1_7g",
-authDomain: "project-1-ec39b.firebaseapp.com",
-databaseURL: "https://project-1-ec39b.firebaseio.com",
-projectId: "project-1-ec39b",
-storageBucket: "project-1-ec39b.appspot.com",
-messagingSenderId: "90657483363"
-};
-firebase.initializeApp(config);
+function getSelected (s, r) {
+    var items = r[s].items;
 
-var database = firebase.database();
+    for (var i = 0; i < items.length; i++) {
 
-var connected = database.ref(".info/connected");
-var people = database.ref("/people");
+        switch (s) {
+            case 'tracks':
+            var a = items[i].name;
+            var b = items[i].artists[0].name;
+            var c = items[i].album.name;
+                break;
+    
+            case 'artists':
+            var a = items[i].name;
+            var b = items[i].genres.join(', ');
+            var c = items[i].followers.total;
+                break;
+    
+    
+            case 'albums':
+            var a = items[i].name;
+            var b = items[i].artists[0].name;
+            var c = items[i].total_tracks;
+                break;
+    
+    
+            case 'playlists':
+            var a = items[i].name;
+            var b = items[i].owner.display_name;
+            var c = items[i].tracks.total;
+                break;
+        }
 
-var name = sessionStorage.getItem('name');
+        var button = $('<button class="each-search" data-number="' + i + '">');
+        var h4 = $('<h4>');
+        var h5 = $('<h5>');
+        var h6 = $('<h6>');
 
+        h4.text(a);
+        h5.text(b);
+        h6.text(c);
 
-var connected = database.ref(".info/connected");
-var people = database.ref("/people");
+        $(button).append(h4, h5, h6);
 
-connected.on("value", function (snapshot) {
-
-if (snapshot.val()) {
-var con = people.push(name);
-con.onDisconnect().remove();
-
-}
-});
-
-people.on("child_removed", function (snapshot) {
-console.log(snapshot.val());
-$('.users p:contains("' + snapshot.val() + '")').remove();
-})
-
-
-people.on("child_added", function (snapshot) {
-var name = snapshot.val();
-
-var p = $('<p>');
-
-p.text(name);
-
-$('.users').append(p);
-
-console.log(people);
-});
-
-var current_url = $(location).attr('href');
-if (current_url.includes('access_token')) {
-console.log("HOLA");
-current_url_array = current_url.split('=');
-current_url_array = current_url_array[1].split('&');
-token = current_url_array[0];
-console.log(token);
-
-}
-
-
-$('#login').click(function () {
-location.href = 'https://accounts.spotify.com/authorize/?client_id=537b0cdacc7f46de8bd9bf27057c93ca&response_type=token&redirect_uri=https://chapaglaura.github.io/Project-1/player.html';
-})
-
-$('.search-btn').click(function () {
-var clicked = $(this).text();
-
-$('.search-parameter').attr({ 'id': clicked.toLowerCase(), 'placeholder': clicked });
-
-$('.search-btn').removeClass('active');
-$(this).addClass('active');
-});
-
-var selected;
-$('.final-search-btn').click(function () {
-selected = $('.search-btn.active').text().toLowerCase();
-var query = $('.search-parameter').val();
-$('.search-parameter').val('');
-query = query.split(' ').join('+');
-
-$('.search-results').empty();
-
-$.ajax({
-url: 'https://api.spotify.com/v1/search?q=' + query + '&type=' + selected + '&access_token=' + token,
-method: 'GET'
-}).then(function (response) {
-r = { ...response };
-selected += 's';
-
-switch (selected) {
-case 'tracks':
-getTracks(response);
-break;
-
-case 'artists':
-break;
-
-
-case 'albums':
-break;
-
-
-case 'playlist':
-break;
-}
-});
-
-
-});
-
-$('.search-results').on('click', '.each-track', function (e) {
-e.preventDefault();
-console.log("HEY");
-var index = $(this).attr('data-number');
-
-switch (selected) {
-case 'tracks':
-setTracks(index);
-break;
-
-case 'artists':
-break;
-
-
-case 'albums':
-break;
-
-
-case 'playlist':
-break;
-}
-});
-
-
-
-});
-
-function getTracks(r) {
-var items = r.tracks.items;
-
-for (var i = 0; i < items.length; i++) {
-var track = items[i].name;
-var artist = items[i].artists[0].name;
-var album = items[i].album.name;
-
-var button = $('<button class="each-track" data-number="' + i + '">');
-var div = $('<div>')
-var h4 = $('<h4>');
-var h5 = $('<h5>');
-var h6 = $('<h6>');
-var hr = $('<hr>')
-
-h4.text(track);
-h5.text(artist);
-h6.text(album);
-
-$(div).append(h4, h5, h6, hr);
-
-$(button).append(div);
-$('.search-results').append(button);
-}
-}
-
-function setTracks(index) {
-
-var trackID = r.tracks.items[index].id;
-
-$.ajax({
-url: 'https://api.spotify.com/v1/me/player/play?access_token=' + 'BQB4P4yYhV1lQ8mm8h_P3eciMSqTaliKsqEPclf-lz4dBvcW-2oLJFN0MB_-qvygmDqoK3JwiCLgk0cTD4TRQOlZYgSujVxodRkoAEVgFNNMpNPmFNYv8ufJyav1FzTQACphl6U-H9kmkZQWhLYQvo0Yx5m_UsPrzs9fiLHwMA',
-method: 'PUT',
-body: {"uris": ["spotify:track:" + trackID]}
-}).then(function (response) {
-console.log(response);
-});
+        $('.search-results').append(button);
+    }
 }
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,75 +1,179 @@
+var current_room;
 $(document).ready(() => {
 
-    sessionStorage.clear();
-    
+    sessionStorage.clear(); // clears session storage
+
+
+    // Sets inputs width styling, considering icon size
     var w = $('.rooms i').innerWidth();
-    
+
     var w_rooms = $('.rooms div').width();
-    
-    
+
     $('.rooms input').width(w_rooms - w - w);
-    
-    
+
+    // Database initialization
     var config = {
-    apiKey: "AIzaSyBx9Tndm3dCv-AKVPS_pslFRAWz3je1_7g",
-    authDomain: "project-1-ec39b.firebaseapp.com",
-    databaseURL: "https://project-1-ec39b.firebaseio.com",
-    projectId: "project-1-ec39b",
-    storageBucket: "project-1-ec39b.appspot.com",
-    messagingSenderId: "90657483363"
+        apiKey: "AIzaSyBx9Tndm3dCv-AKVPS_pslFRAWz3je1_7g",
+        authDomain: "project-1-ec39b.firebaseapp.com",
+        databaseURL: "https://project-1-ec39b.firebaseio.com",
+        projectId: "project-1-ec39b",
+        storageBucket: "project-1-ec39b.appspot.com",
+        messagingSenderId: "90657483363"
     };
     firebase.initializeApp(config);
-    
-    var database = firebase.database();
-    
-    var people = database.ref('/people');
-    
-    $('#your-name').on('input', function () {
-    var name = $(this).val();
-    console.log(name);
-    people.once("value", function (snapshot) {
-    
-    if (snapshot.numChildren() > 0) {
-    var a = Object.values(snapshot.val());
-    console.log(a.indexOf(name));
-    
-    if (a.indexOf(name) === -1) {
-    $('#valid-username').css('color', 'lightgreen').text('Valid username.');
-    $('#name-button').removeAttr('disabled');
-    }
-    else {
-    $('#valid-username').css('color', 'lightgray').text('That name is already taken. Choose a different one.');
-    $('#name-button').attr('disabled', 'true');
-    }
-    }
-    else {
-    $('#valid-username').css('color', 'lightgreen').text('Valid username.');
-    $('#name-button').removeAttr('disabled');
-    }
-    
+
+    var database = firebase.database(); // store firebase database
+
+    var rooms = database.ref('/rooms'); // reference to rooms directory in database
+
+    /* Checks if there are previously created rooms.
+    If so, joining is available. */
+    rooms.once("value", function (snapshot) {
+        if (snapshot.val() === null) {
+            $('#join-room-name').attr('disabled', 'true');
+        }
+        else {
+            $('#join-room-name').removeAttr('disabled');
+        }
     });
-    
+
+    // Input event handler
+    $('#create-room-name').on('input', function () {
+        var name = $(this).val(); // stores value in text input in variable
+
+        /* Checks if room name chosen to create already exists,
+         so there won't be two rooms with the same name.
+         (1) If it is not the first room created, proceed to name check.
+         (2) Creates array of the room names stored in rooms directory in database.
+         (3) If name is not taken, alert the user so, and viceversa.
+         (4) If it is the first room ever created, name check is not necessary.*/
+
+        rooms.once("value", function (snapshot) {
+
+            if (snapshot.val() != null) { // (1)
+                var a = Object.keys(snapshot.val()); // (2)
+
+                if (a.indexOf(name) === -1) { // (3)
+                    $('#valid-room').css('color', 'lightgreen').text('Valid room.');
+                    $('#create-button').removeAttr('disabled');
+                }
+                else {
+                    $('#valid-room').css('color', 'lightgray').text('That room already exists. Choose a different name.');
+                    $('#create-button').attr('disabled', 'true');
+                }
+            }
+            else { // (4)
+                $('#valid-room').css('color', 'lightgreen').text('Valid room.');
+                $('#create-button').removeAttr('disabled');
+            }
+        });
     });
-    
-    
-    $('#name-button').click(function () {
-    var name = $('#your-name').val();
-    sessionStorage.setItem('name', name);
-    location.href = 'player.html';
+
+    // Click event handler
+    $('#create-button').click(function () {
+
+        /* (1) Retrieves text from input and stores it in variable.
+           (2) Reference to room in rooms directory in database.
+           (3) Store room name in session storage for later use. */
+        var room_name = $('#create-room-name').val(); // (1)
+        current_room = database.ref('/rooms/' + room_name); // (2)
+        sessionStorage.setItem('room', room_name); // (3)
+
     });
-    
-    
-    
+
+    //Input event handler
+    $('#join-room-name').on('input', function () {
+        var name = $(this).val(); // stores value in text input in variable
+
+        /* Checks if room name chosen to join does exist.
+         A non-existent room cannot be joined (duh).
+         (1) If there is at least one room in database, proceed to name check.
+         (2) Creates array of the room names stored in rooms directory in database.
+         (3) If name matches an existing room name, alert the user so, and viceversa.
+         (4) If there are no rooms, name check is not necessary, thus joining is unavailable.*/
+        rooms.once("value", function (snapshot) {
+
+            if (snapshot.val() != null) { // (1)
+                var a = Object.keys(snapshot.val()); // (2)
+
+                if (a.indexOf(name) === -1) { // (3)
+                    $('#existing-room').css('color', 'lightgray').text('Room does not exist. Choose a different name.');
+                    $('#join-button').attr('disabled', 'true');
+                }
+                else { 
+                    $('#existing-room').css('color', 'lightgreen').text('Valid room.'); 
+                    $('#join-button').removeAttr('disabled');
+                }
+            }
+        });
+    });
+
+    // Click event handler
+    $('#join-button').click(function () {
+
+        /* (1) Retrieves text from input and stores it in variable.
+           (2) Reference to room in rooms directory in database.
+           (3) Store room name in session storage for later use. */
+        var room_name = $('#join-room-name').val(); // (1)
+        current_room = database.ref('/rooms/' + room_name); // (2)
+        sessionStorage.setItem('room', room_name); // (3)
+    });
+
+    // Click event handler
     $('.rooms button').click(function () {
-    $('.main-row').css('left', '-100%');
-    $('.secondary-row').css('left', '0');
-    })
-    
-    $('#create-button').click(function() {
-    var room_name = $('#create-room-name').val();
-    })
-    
-    
-    })
-    
-    
+        // Page transition
+        $('.main-row').css('left', '-100%');
+        $('.secondary-row').css('left', '0');
+    });
+
+    //Input event handler
+    $('#your-name').on('input', function () {
+        var name = $(this).val(); //store value in text input
+
+        /* Checks if name chosen is already taken in
+        room created/joined, so there won't be two names
+        repeated. 
+        (1) If there are people inside specified room, proceed to name check.
+        (2) Creates array of the room names stored in rooms directory in database.
+        (3) Stores flag for room admin in session storage for later use
+        (4) If name is not taken in room, alert the user so, and viceversa.
+        (5) If the room is empty, name check is not necessary.*/
+
+        current_room.once("value", function (snapshot) {
+            if (snapshot.val() != null) { // (1)
+                var a = Object.values(snapshot.val()); // (2)
+                console.log(a.indexOf(name));
+                sessionStorage.setItem('created', 'no'); // (3)
+                if (a.indexOf(name) === -1) { // (4)
+                    $('#valid-username').css('color', 'lightgreen').text('Valid username.');
+                    $('#name-button').removeAttr('disabled');
+                }
+                else {
+                    $('#valid-username').css('color', 'lightgray').text('That name is already taken. Choose a different one.');
+                    $('#name-button').attr('disabled', 'true');
+                }
+            }
+            else { // (5)
+                sessionStorage.setItem('created', 'yes');
+                $('#valid-username').css('color', 'lightgreen').text('Valid username.');
+                $('#name-button').removeAttr('disabled');
+            }
+
+        });
+
+    });
+
+    // Click event handler
+    $('#name-button').click(function () {
+
+        /* (1) Retrieves name from input and stores it in variable.
+           (2) Stores username in session storage for later use.
+           (3) Redirect to player webpage. */
+        var name = $('#your-name').val(); // (1)
+        sessionStorage.setItem('name', name); // (2)
+        location.href = 'player.html'; // (3)
+    });
+
+
+})
+
